fda551e9bd9c7dcc54797c3578ba96865669fc77
Golo Roden <webmaster@goloroden.de>
2015-09-24 15:16:35 +0200

    Tried to remove dependency to local gulp.

--- bin/bot.js
+++ bin/bot.js
@@ -6,48 +6,53 @@ const fs = require('fs'),
     path = require('path');
 
 const buntstift = require('buntstift'),
-    shell = require('shelljs'),
+    gulp = require('gulp'),
     updateNotifier = require('update-notifier');
 
 const packageJson = require('../package.json');
 
-const gulp = path.join(__dirname, '..', 'node_modules', '.bin', 'gulp'),
-    gulpfile = path.join(process.cwd(), 'roboter.js');
+const gulpfile = path.join(process.cwd(), 'roboter.js');
 
-const args = process.argv.slice(2).join(' ');
+const args = process.argv.slice(2);
 
 updateNotifier({
   packageName: packageJson.name,
   packageVersion: packageJson.version
 }).notify();
 
-/* eslint-disable no-process-exit */
-if (!fs.existsSync(gulp)) {
-  buntstift.error('roboter is not installed locally.');
-  buntstift.newLine();
-  buntstift.info('Please run the following command:');
-  buntstift.newLine();
-  buntstift.info('  npm install roboter --save-dev --save-exact');
-  process.exit(1);
-}
-
 if (!fs.existsSync(gulpfile)) {
   buntstift.error('roboter.js is missing.');
-  process.exit(1);
+  buntstift.exit(1);
 }
+
+require(gulpfile);
 
 if (args.length === 0) {
-  const result = shell.exec(`${gulp} --gulpfile ${gulpfile} --color true --tasks-simple`, { silent: true });
-
-  const tasks = result.output.split('\n').filter(task => task && !task.startsWith('_')).sort();
-
-  buntstift.success('The following tasks are availabe:');
-  tasks.forEach(task => {
-    buntstift.list(task);
+  return gulp.start('analyze', function () {
+    buntstift.exit();
   });
 
-  process.exit(0);
+  // const result = shell.exec(`${gulp} --gulpfile ${gulpfile} --color true --tasks-simple`, { silent: true });
+  //
+  // const tasks = result.output.split('\n').filter(task => task && !task.startsWith('_')).sort();
+  //
+  // buntstift.success('The following tasks are availabe:');
+  // tasks.forEach(task => {
+  //   buntstift.list(task);
+  // });
+  //
+  // process.exit(0);
 }
 
-process.exit(shell.exec(`${gulp} --gulpfile ${gulpfile} --color true ${args}`).code);
-/* eslint-enable no-process-exit */
+gulp.on('task_start', e => {
+  buntstift.info(`${e.task} started...`);
+});
+
+gulp.start(args[0], function (err) {
+  if (err) {
+    buntstift.error('Roboter sad.', { prefix: ':[' });
+    buntstift.exit(1);
+  }
+
+  buntstift.success('Roboter happy.', { prefix: ':]' });
+});
