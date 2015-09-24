d44cb95cbd82b23a3e898336fa3550eace9ee871
Matthias Wagler <wagler@intuity.de>
2015-09-24 12:20:40 +0200

    Added default task.

--- bin/bot.js
+++ bin/bot.js
@@ -12,5 +12,15 @@ const gulpfile = path.join(process.cwd(), 'roboter.js');
 const args = process.argv.slice(2).join(' ');
 
 /* eslint-disable no-process-exit */
+if (args.length === 0) {
+  const result = shell.exec(`${gulp} --gulpfile ${gulpfile} --color true --tasks-simple`, { silent: true });
+
+  const tasks = result.output.split('\n').filter(task => task && !task.startsWith('_')).join('\n');
+
+  console.log(tasks);
+
+  process.exit(0);
+}
+
 process.exit(shell.exec(`${gulp} --gulpfile ${gulpfile} --color true ${args}`).code);
 /* eslint-enable no-process-exit */
