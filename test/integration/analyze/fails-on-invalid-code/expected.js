4b352f35ce9f9217b11763ca878c927cf5347a01
Golo Roden <webmaster@goloroden.de>
2016-11-11 16:29:20 +0100

    Update dependencies.

--- test/integration/analyze/fails-on-invalid-code/expected.js
+++ test/integration/analyze/fails-on-invalid-code/expected.js
@@ -3,7 +3,7 @@
 const exitCode = 1;
 
 const stdout = `
-  3:7  error  'x' is defined but never used  no-unused-vars
+  3:7  error  'x' is assigned a value but never used  no-unused-vars
 
 ✖ 1 problem (1 error, 0 warnings)
 `;
