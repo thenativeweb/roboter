fcf616ecff02751ca4215db5f9038ab09f312c7b
Matthias Wagler <wagler@intuity.de>
2017-12-01 17:57:40 +0100

    Add tests and license details to license task.

--- configuration/packageLicenses.js
+++ configuration/packageLicenses.js
@@ -6,9 +6,19 @@ const packageLicenses = {
     '0.0.4': 'MIT'
   },
 
+  coa: {
+    // https://github.com/veged/coa/blob/v1.0.4/package.json
+    '1.0.4': 'MIT'
+  },
+
   duplexer2: {
     // https://github.com/deoxxa/duplexer2/blob/0.0.2/LICENSE.md
     '0.0.2': 'BSD-3-Clause'
+  },
+
+  entities: {
+    // https://github.com/fb55/entities/blob/v1.1.1/LICENSE
+    '1.1.1': 'BSD-3-Clause'
   },
 
   esquery: {
@@ -26,7 +36,26 @@ const packageLicenses = {
     '1.0.1': 'MIT'
   },
 
+  inherits: {
+    // https://github.com/isaacs/inherits/blob/v1.0.1/LICENSE
+    // Version 1.0.2 has not been tagged on Github.
+    '1.0.2': 'ISC'
+  },
+
+  'json-schema': {
+    // https://github.com/kriszyp/json-schema/blob/81ca359daeea643019a4ee81b7a57c06ac53d800/README.md
+    '0.2.3': 'BSD-3-Clause'
+  },
+
+  nopter: {
+    // https://github.com/stevenvachon/nopter/blob/bb0478aa64b6a1a156c28cfc51e94788617d6c0f/package.json
+    '0.3.0': 'MIT'
+  },
+
   'regenerator-transform': {
+    // https://github.com/facebook/regenerator/blob/85c9e43331576be96e5dcc61757995397ab15b77/LICENSE
+    '0.9.11': 'BSD-2-Clause',
+
     // https://github.com/facebook/regenerator/blob/30d34536b9e3f7a2873b04a16ec66fec9c8246f6/LICENSE
     '0.10.1': 'BSD-2-Clause'
   },
@@ -34,6 +63,11 @@ const packageLicenses = {
   regjsparser: {
     // https://github.com/jviereck/regjsparser/blob/0.1.5/LICENSE.BSD
     '0.1.5': 'BSD-2-Clause'
+  },
+
+  semver: {
+    // https://github.com/npm/node-semver/blob/v4.3.2/LICENSE
+    '4.3.2': 'BSD-2-Clause'
   },
 
   'unique-stream': {
---
+++ test/integration/license/gets-license-from-license-files/expected.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const exitCode = 1;
+
+const stdout = ``;
+
+const stderr = '✗ License check failed.';
+
+module.exports = { exitCode, stdout, stderr };
