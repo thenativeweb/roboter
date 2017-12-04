5913e523336922297641606ca179348149da870e
Matthias Wagler <wagler@intuity.de>
2017-12-04 10:21:24 +0100

    Include feedback from review.

--- configuration/licenseHeuristics.js
+++ configuration/licenseHeuristics.js
@@ -2,7 +2,7 @@
 
 const licenseHeuristics = {
   'Apache-2.0*': /Apache License, Version 2\.0/,
-  'GPL-3.0*': /GNU GENERAL PUBLIC LICENSE.*\n.*Version 3,/m,
+  'GPL-3.0*': /GNU GENERAL PUBLIC LICENSE\s*Version 3,/m,
   'MIT*': /MIT|The MIT License|ermission is hereby granted, free of charge, to any/
 };
 
