c81eb0f933ffea24127c5348911ac99618c1474a
Matthias Wagler <wagler@intuity.de>
2018-03-26 10:44:24 +0200

    Add check for unused dependencies to release.

---
+++ test/integration/release/warns-on-outdated-dependencies/expected.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const exitCode = 0;
+
+const stdout = `Package  Current  Wanted  Latest  Location
+express  MISSING   1.0.0`;
+
+const stderr = '';
+
+module.exports = { exitCode, stdout, stderr };
