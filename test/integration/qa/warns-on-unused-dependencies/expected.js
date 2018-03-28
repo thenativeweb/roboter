4e1fcfcdaa99f5d40a35abd43d621fdd6fa0530a
Matthias Wagler <wagler@intuity.de>
2018-03-28 17:40:33 +0200

    Add a qa task.

---
+++ test/integration/qa/warns-on-unused-dependencies/expected.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const exitCode = 0;
+
+const stdout = [
+  'Running unused or missing dependencies check',
+  `Following dependencies are unused or missing:
+
+noop3`
+];
+
+const stderr = ``;
+
+module.exports = { exitCode, stdout, stderr };
