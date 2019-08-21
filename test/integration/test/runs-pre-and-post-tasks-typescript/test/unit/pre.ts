32736cbe6e85023d9630f5496563a70e5b04e650
Hannes Leutloff <hannes.leutloff@thenativeweb.io>
2019-08-21 18:13:16 +0200

    use node and ts-node to run pre/post test tasks

--- test/integration/test/runs-pre-and-post-tasks-typescript/test/unit/pre.ts
+++ test/integration/test/runs-pre-and-post-tasks-typescript/test/unit/pre.ts
@@ -1,7 +1,3 @@
-const pre = async function (): Promise<void> {
-  /* eslint-disable no-console */
-  console.log('Hello from pre task!');
-  /* eslint-enable no-console */
-};
-
-export default pre;
+/* eslint-disable no-console */
+console.log('Hello from pre task!');
+/* eslint-enable no-console */
--- test/integration/test/runs-pre-and-post-tasks-with-context-typescript/lib/external.ts
+++ test/integration/test/runs-pre-and-post-tasks-with-context-typescript/lib/external.ts
@@ -1,3 +1,3 @@
-export default function (): void {
+export default async function (): Promise<void> {
     console.log('Hello from external!');
 };
