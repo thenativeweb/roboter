0338ef1d08c48dabee4ef188e97732a3dd141536
Hannes Leutloff <hannes.leutloff@thenativeweb.io>
2019-08-28 12:52:01 +0200

    Check for unused dependencies in TypeScript projects. (#86)

---
+++ test/integration/deps/does-not-warn-if-everything-is-fine-typescript/expected.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const exitCode = 0;
+
+const stdout = [
+  `▸ Running unused or missing dependencies check...
+─`,
+  `▸ Running outdated dependencies check...
+─`,
+  'Dependency check successful.'
+];
+
+const stderr = '';
+
+module.exports = { exitCode, stdout, stderr };
