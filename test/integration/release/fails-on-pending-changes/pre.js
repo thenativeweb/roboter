b2566447fb03f921e43efdf400497bad21cfc133
Golo Roden <golo.roden@thenativeweb.io>
2019-11-29 11:40:44 +0100

    BREAKING CHANGE: Remove release command. (#183)
    * Remove release command.
    
    * Clean up and prepare for semantic-release.
    
    * Remove unneeded error.

--- test/integration/release/fails-on-pending-changes/pre.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const shell = require('shelljs');
-
-shell.exec('echo "second file" > second.txt');
-shell.exec('git add .');
--- test/integration/release/shows-new-version/expected.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const exitCode = 0;
-
-const stdout = 'Successfully released version 0.0.2.';
-
-const stderr = '';
-
-module.exports = { exitCode, stdout, stderr };
