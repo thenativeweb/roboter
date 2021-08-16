ee431ea8ba873a979b7b0b6618f09516fe2d90b9
Hannes Leutloff <hannes.leutloff@thenativeweb.io>
2021-08-16 15:08:00 +0200

    fix: Always use ANSI colors in output. (#687)

--- lib/bin/roboter.ts
+++ lib/bin/roboter.ts
@@ -1,11 +1,17 @@
 #!/usr/bin/env -S node --loader ts-node/esm --experimental-specifier-resolution=node --no-warnings
 
-import { buntstift } from 'buntstift';
 import { getHandlers } from '../cli/getHandlers';
 import { rootCommand } from '../cli/rootCommand';
 import { runCli } from 'command-line-interface';
+import { buntstift, ColorLevel } from 'buntstift';
 
 try {
+  buntstift.configure(
+    buntstift.
+      getConfiguration().
+      withColorLevel(ColorLevel.Ansi)
+  );
+
   await runCli({
     rootCommand: rootCommand(),
     argv: process.argv,
