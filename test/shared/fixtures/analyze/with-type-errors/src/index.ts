4905ce5c124b455d873914ff6f5654f0479fea31
Noah Hummel <strangedev@posteo.net>
2021-09-02 17:26:39 +0200

    chore: rename schmarrn to incorrect

--- test/shared/fixtures/analyze/with-type-errors/src/index.ts
+++ test/shared/fixtures/analyze/with-type-errors/src/index.ts
@@ -4,7 +4,7 @@ const add = function (left: number, right: number): number {
 
 const sum = add(23, 42);
 
-const schmarrn = add('left', 'right');
+const incorrect = add('left', 'right');
 
 // eslint-disable-next-line no-console
-console.log(sum, schmarrn);
+console.log(sum, incorrect);
